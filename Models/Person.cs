16675d8f4b5d565dbddbd1ce627abf3003265a22
anaBeatrizWalker <anabiawalker@gmail.com>
2024-08-07 19:12:15 -0300

    Adicionando classe Person e suas propriedades

---
+++ Models/Person.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExplorandoCSharp.Models
+{
+    public class Person
+    {
+        //Encapsulamento (criando campos para armazenar as propriedades)
+        private string _name;
+        private int _age;
+
+        //Propriedades
+        public string Name 
+        { 
+            get
+            {
+                return _name.ToUpper();
+            }
+            //Body Expression: get => _name.ToUpper();
+
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("The name cannot be empty. Closing the program...");
+                }
+                _name = value;
+            }
+        }
+
+        public string Surname { get; set; }
+
+        //Propriedade somente leitura
+        public string CompleteName => $"{Name} {Surname}";
+
+        public int Age 
+        { 
+            //Body Expression:
+            get => _age;
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("The age cannot be less than zero. Closing the program..");
+                }
+                _age = value;
+            }
+        }
+
+        //Métodos
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hi! My name is {CompleteName} and I have {Age} years.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExplorandoCSharp.Models;
+
+
+Person p = new Person();
+p.Name="Ana";
+p.Surname="Walker";
+p.Age=20;
+p.IntroduceYourself();
