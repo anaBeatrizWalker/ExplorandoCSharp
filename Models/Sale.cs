e38bad295ddff16bb676f73db781db14fc3a5ea8
anaBeatrizWalker <anabiawalker@gmail.com>
2024-08-12 19:18:56 -0300

    Adicionando atributo JsonProperty

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ExplorandoCSharp.Models
 {
@@ -16,6 +17,8 @@ namespace ExplorandoCSharp.Models
         }
 
         public int Id { get; set; }
+
+        [JsonProperty("Product_Name")]
         public string Product { get; set; }
         public decimal Price { get; set; }
         public DateTime Date { get; set; }
